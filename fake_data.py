2b1921780554255602ecb76771908b40dc8d7216
Dymasius Sitepu <dymasius@Dymasiuss-MacBook-Air.local>
2022-10-01 23:52:05 +0800

    15:35 fake_data.py created made on

--- fake_data.py
+++ fake_data.py
@@ -1,3 +1,24 @@
 import pandas as pd
 from faker import Faker
 
+# create the fake data using faker
+fake = Faker()
+profiles = [fake.profile() for i in range(10)]
+
+# store the fake profile data with pandas
+df = pd.DataFrame(profiles)
+df = df[['name', 'mail', 'address']]
+
+# create the phone number and comment fake data
+numbers = [fake.phone_number() for i in range(0,10)]
+comment = '-'
+
+#store the data at the dataframe 
+df['phone number'] = numbers
+df['comments'] = comment
+
+#change the column name
+df.rename(columns={'name': 'Name', 'mail':'Email', 'address':'Address'}, inplace=True)
+
+# export the data to CSV
+df.to_csv('fake_data.csv', index=False)
