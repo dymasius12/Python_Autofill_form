bc9f59977f8d8f894064c876ffda8484bd0b0f0e
Dymasius Sitepu <dymasius@Dymasiuss-MacBook-Air.local>
2022-10-01 23:06:02 +0800

    10:49 successfully able to run chromedriver, chrome is controlled by python made on

--- automate_form.py
+++ automate_form.py
@@ -1,5 +1,7 @@
+from operator import contains
 from selenium import webdriver 
 from selenium.webdriver.chrome.service import Service 
+import time 
 
 path = '/Users/dymasius/Sandbox/Python-Dev/development/python_autofill_form/chromedriver'
 service = Service(executable_path = path)
@@ -7,3 +9,11 @@ website = 'https://forms.gle/CJf4QapkiA7s7HcZ7'
 driver = webdriver.Chrome(service=service)
 
 driver.get(website)
+time.sleep(3)
+
+value = '//div[contains(@data-params, "Address")]//input | //div[contains(@data-params, "Address")]//textarea'
+
+# Basically trying to find the element
+text_input = driver.find_element(by='xpath', value=value)
+
+text_input.send_keys('Hello_World!')
