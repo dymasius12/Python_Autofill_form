272992871c2eef907bacbe8b364f9fcc7bdfb82e
Dymasius Sitepu <dymasius@Dymasiuss-MacBook-Air.local>
2022-10-01 23:23:54 +0800

    14:26 Created fake_data.py made on

--- automate_form.py
+++ automate_form.py
@@ -11,9 +11,14 @@ driver = webdriver.Chrome(service=service)
 driver.get(website)
 time.sleep(3)
 
-value = '//div[contains(@data-params, "Address")]//input | //div[contains(@data-params, "Address")]//textarea'
-
+# inputing the value for the find_element
+value_input = '//div[contains(@data-params, "Address")]//input | //div[contains(@data-params, "Address")]//textarea'
+value_submit = '//div[@role="button"]//span[text()="Submit"]'
 # Basically trying to find the element
-text_input = driver.find_element(by='xpath', value=value)
-
+text_input = driver.find_element(by='xpath', value=value_input)
 text_input.send_keys('Hello_World!')
+
+submit_button = driver.find_element(by='xpath', value=value_submit)
+submit_button.click()
+
+driver.quit()
